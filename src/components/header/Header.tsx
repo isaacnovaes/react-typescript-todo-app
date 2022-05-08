f53896389178f51769e87789ed5a458bdf792c9e
Isaac Novaes <isaac.novaes@hotmail.com>
2022-05-08 23:03:38 +0200

    Implement theme toggling fuctionality

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import styles from "./Header.module.scss";
+import { Context } from "../../context/ContextProvider";
+import useTheme from "../../hooks/themeHook";
+
+function Header() {
+	const { dispatch } = React.useContext(Context);
+	const isDarkTheme = useTheme();
+	const buttonClasses = isDarkTheme ? "" : styles.Light;
+
+	return (
+		<header className={styles.Header}>
+			<h1>TODO</h1>
+			<button
+				className={buttonClasses}
+				type="button"
+				onClick={() => dispatch({ type: "toggle" })}
+			></button>
+		</header>
+	);
+}
+
+export default Header;
