62ca1ac22c91c45d9077d7b04deb4da1f0c77dfb
Isaac Novaes <isaac.novaes@hotmail.com>
2022-05-08 22:25:13 +0200

    Change the context import and add font weight variables

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ContextProvider } from "./context/ContextProvider";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<ContextProvider>
+			<App />
+		</ContextProvider>
+	</React.StrictMode>
 );
-
