e23a18be2ffdfb293ad7d2df712b4d96dce97e86
Isaac Novaes <isaac.novaes@hotmail.com>
2022-05-08 23:05:19 +0200

    Update App component styles and import AppBackground, Header, MainContainer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+// type TodoType = { id: number; todo: string };
+// type TodosType = TodoType[];
+// const DUMMY: TodosType = [
+// 	{ id: 84, todo: "Complete JavaScript course" },
+// 	{ id: 4, todo: "Jog around the park 3x" },
+// 	{ id: 8, todo: "10 minutes meditation" },
+// 	{ id: 5, todo: "Read for one hour" },
+// 	{ id: 7, todo: "Pick up groceries" },
+// ];
+// import * as React from "react";
+import styles from "./App.module.scss";
+import AppBackground from "./components/appBackground/AppBackground";
+import Header from "./components/header/Header";
+import MainContainer from "./components/mainContainer/MainContainer";
+// import { Context } from "./context/ContextProvider";
+
+import useTheme from "./hooks/themeHook";
+
 function App() {
-	return <div className="App">Hello</div>;
+	const isDarkTheme = useTheme();
+
+	const classes = `${styles.App} ${isDarkTheme ? "" : styles.Light}`;
+
+	return (
+		<div className={classes}>
+			<AppBackground />
+			<MainContainer>
+				<Header />
+			</MainContainer>
+		</div>
+	);
 }
 
 export default App;
