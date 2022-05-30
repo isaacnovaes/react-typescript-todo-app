954c0def635a35f484860de64103d43dcd0b9255
Isaac Novaes <isaac.novaes@hotmail.com>
2022-05-30 11:53:32 +0200

    Update CreateTodo component

--- src/components/CreateTodo/CreateTodo.tsx
+++ src/components/CreateTodo/CreateTodo.tsx
@@ -1,21 +1,37 @@
 import styles from "./CreateTodo.module.scss";
 import * as React from "react";
 import useTheme from "../../hooks/themeHook";
+import { Context } from "../../context/ContextProvider";
 
-interface CreateTodoProps {
-	onCreateCompleteTodo: () => void;
-	onEnterTodo: (event: React.KeyboardEvent) => void;
-	createCompletedTodo: boolean;
-	inputRef: React.RefObject<HTMLInputElement>;
-}
-
-export default function CreateTodo({
-	onCreateCompleteTodo,
-	onEnterTodo,
-	createCompletedTodo,
-	inputRef,
-}: CreateTodoProps) {
+export default function CreateTodo() {
 	const isDark = useTheme();
+
+	const { dispatch } = React.useContext(Context);
+
+	const inputRef = React.useRef<HTMLInputElement>(null);
+
+	const [createCompletedTodo, setCreateCompletedTodo] = React.useState(false);
+
+	const onAddTodo = React.useCallback(
+		(event: React.KeyboardEvent) => {
+			if (!inputRef.current || !inputRef.current.value.trim()) return;
+
+			if (event.key !== "Enter") return;
+
+			dispatch({
+				type: "add todo",
+				todo: inputRef.current.value.trim(),
+				complete: createCompletedTodo,
+			});
+
+			inputRef.current.value = "";
+		},
+		[createCompletedTodo, dispatch]
+	);
+
+	const onCreateCompleteTodo = React.useCallback(() => {
+		setCreateCompletedTodo(current => !current);
+	}, []);
 
 	return (
 		<div
@@ -29,7 +45,7 @@ export default function CreateTodo({
 			<input
 				type="text"
 				placeholder="Create a new todo..."
-				onKeyUp={onEnterTodo}
+				onKeyUp={onAddTodo}
 				ref={inputRef}
 			/>
 		</div>
