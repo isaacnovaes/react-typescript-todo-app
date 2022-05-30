5b1eefad99d6834d55dcbc3bbe01f21583322d7b
Isaac Novaes <isaac.novaes@hotmail.com>
2022-05-30 11:54:18 +0200

    Update TodoItem component

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,14 +1,11 @@
+import * as React from "react";
 import styles from "./TodoItem.module.scss";
 import useTheme from "../../hooks/themeHook";
+import { Context } from "../../context/ContextProvider";
 
-function TodoItem(Todo: {
-	id: number;
-	todo: string;
-	complete: boolean;
-	onClose: (todoID: number) => void;
-	onToggle: (todoID: number) => void;
-}) {
+function TodoItem(Todo: { id: number; todo: string; complete: boolean }) {
 	const isDark = useTheme();
+	const { dispatch } = React.useContext(Context);
 
 	return (
 		<div
@@ -20,7 +17,9 @@ function TodoItem(Todo: {
 				className={`${styles.CreateTudoToggleButton} ${
 					Todo.complete ? styles.CompletedTodo : ""
 				}`}
-				onClick={() => Todo.onToggle(Todo.id)}
+				onClick={() =>
+					dispatch({ type: "toggle todo complete", todoID: Todo.id })
+				}
 			></button>
 			<p className={`${Todo.complete ? styles.TodoCompleted : " "}`}>
 				{Todo.todo}
@@ -28,7 +27,7 @@ function TodoItem(Todo: {
 			<button
 				type="button"
 				className={styles.CloseTodo}
-				onClick={() => Todo.onClose(Todo.id)}
+				onClick={() => dispatch({ type: "remove todo", todoID: Todo.id })}
 			></button>
 		</div>
 	);
