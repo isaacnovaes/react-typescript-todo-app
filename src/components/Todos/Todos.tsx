4df509e089259b1c3bfb4e45b0bfb35b28b570ad
Isaac <isaac.novaes@hotmail.com>
2022-08-24 00:33:47 +0200

    Update drag&drop

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,33 +1,38 @@
-import { Reorder } from 'framer-motion';
-import { useEffect, useState } from 'react';
+import { Reorder, AnimatePresence } from 'framer-motion';
+import { useState, useEffect } from 'react';
 import TodoItem from '../TodoItem/TodoItem';
 import type { TodoInterface } from '../TodoListContainer/TodoListContainer';
 import TodoSummary from '../TodoSummary/TodoSummary';
 import styles from './Todos.module.scss';
 
 export default function Todos({ todos }: { todos: TodoInterface[] }) {
-    // const getItems = todos.map(todos=>todos.todo)
     const [items, setItems] = useState(todos);
-    useEffect(() => setItems(todos), [todos]);
+    useEffect(() => {
+        setItems(todos);
+    }, [todos]);
     return (
         <div className={styles.todos}>
             <Reorder.Group
-                axis='y'
                 values={items}
                 onReorder={setItems}
                 style={{ overflow: 'hidden' }}
+                axis='y'
             >
-                {items.map((todo) => (
-                    <Reorder.Item
-                        key={todo.id}
-                        value={todo}
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}
-                    >
-                        <TodoItem {...todo} />
-                    </Reorder.Item>
-                ))}
+                <AnimatePresence>
+                    {items.map((todo) => (
+                        <Reorder.Item
+                            key={todo.id}
+                            id={todo.id.toString()}
+                            value={todo}
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            whileDrag={{ cursor: 'grab' }}
+                        >
+                            <TodoItem {...todo} />
+                        </Reorder.Item>
+                    ))}
+                </AnimatePresence>
             </Reorder.Group>
             <TodoSummary todos={todos} />
         </div>
